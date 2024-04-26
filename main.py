7a629dcabb530af05292485093452e2f983ece25
vpq5kd <vpq5kd@virginia.edu>
2024-04-25 23:50:38 -0400

    started to write the handle client initial method

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ def create_server():
     server_socket.bind(('127.0.0.1', 1000))
     server_socket.listen(5)
     return server_socket
+
+"""handles the client's connection to the server"""
+def handle_client(chat_member):
+    client_connection = chat_member.socket
+    welcome_message = "Welcome to my chat room, please enter your name:"
+    client_connection.send(welcome_message.encode('utf-8'))
+    client_name = client_connection.recv(1024).decode('utf-8')
+
+
+
 #main:
 def main():
     server_socket = create_server()
@@ -27,6 +37,7 @@ def main():
         client_socket, client_address = server_socket.accept()
         temp = chat_member(client_socket,client_address)
         chat_member_array.append(temp)
+
         print(client_address)
 
 
