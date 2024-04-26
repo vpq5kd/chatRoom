e480af8e65797b572bf98d5b076c8694f105de9f
vpq5kd <vpq5kd@virginia.edu>
2024-04-26 02:06:01 -0400

    wrote client code to handle all the way up to before the start of the chat room

--- client.py
+++ client.py
@@ -3,12 +3,21 @@ import socket
 
 #global variables:
 SERVER_ADDRESS = '127.0.0.1'
-SERVER_PORT = 1000
+SERVER_PORT = 8888
+
+REAL_PERSON = 0
+NOT_REAL_PERSON = 1
 
 #functions:
+
+"""creates socket"""
 def create_socket():
     client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     return client_socket
+
+"""handles a chat between the other client. (empty placeholder"""
+def start_chatting():
+    return
 
 #main:
 def main():
@@ -25,11 +34,36 @@ def main():
         client_name = input().encode('utf-8')
         client_socket.send(client_name)
 
+        #server hello
+        server_hello = client_socket.recv(1024).decode('utf-8')
+        print(server_hello)
+
         #get available members:
         members_list = client_socket.recv(4096).decode('utf-8')
         print(members_list)
+        enter_name = client_socket.recv(1024).decode('utf-8')
+        print(enter_name)
+
+        #'connect' with a client
+
+        connected = False
+        while(not connected):
+            person_to_chat = input().encode('utf-8')
+            client_socket.send(person_to_chat)
+            is_person_real = int(client_socket.recv(1024).decode('utf-8'))
+            if is_person_real == REAL_PERSON:
+                start_session = client_socket.recv(1024).decode('utf-8')
+                print(start_session)
+                start_chatting()
+                break
+            elif is_person_real == NOT_REAL_PERSON:
+                not_real = client_socket.recv(1024).decode('utf-8')
+                print(not_real)
+
+
 
 
     except Exception as e:
+        client_socket.close()
         print(e)
 main()
--- server.py
+++ server.py
@@ -9,6 +9,8 @@ import os
 
 #global variables:
 chat_member_array = []
+ACCEPTED_IP = '0'.encode('utf-8')
+NOT_ACCEPTED_IP = '1'.encode('utf-8')
 
 #classes:
 """defines a chat_member and holds their data for chat room use"""
@@ -22,6 +24,9 @@ class chat_member:
         self.name = name
     def set_is_available(self, is_available):
         self.is_available = is_available
+
+    def get_name(self):
+        return self.name
     def get_name_and_ip(self):
         if self.is_available:
             return f'{self.name} @ {self.address}'
@@ -34,13 +39,13 @@ class chat_member:
 """creates the server"""
 def create_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('127.0.0.1', 1000))
+    server_socket.bind(('127.0.0.1', 8888))
     server_socket.listen(5)
     return server_socket
 """prints the server started and the ip/port it started on"""
 def print_information():
     server_address = '127.0.0.1'
-    server_port = 1000
+    server_port = 8888
     print(f"server started on {server_address} @ {server_port}")
     print("-"*50+"\n")
 
@@ -53,35 +58,40 @@ def handle_client(chat_member):
     welcome_message = "Welcome to my chat room, please enter your name:"
     client_connection.send(welcome_message.encode('utf-8'))
     client_name = client_connection.recv(1024).decode('utf-8')
+    server_hello = f"hello {client_name}! Thanks for joining my chat room :)".encode('utf-8')
+    client_connection.send(server_hello)
     chat_member.set_name(client_name)
 
     #sends the available member info to the client
     chat_member_info = [member.get_name_and_ip() for member in chat_member_array]
     chat_member_as_string = "\n".join(chat_member_info)
-    chat_members = f"Here is the list of people available to chat with:\n{chat_member_as_string}".encode('utf-8')
+    chat_members = f"\nHere is the list of people available to chat with:\n{chat_member_as_string}".encode('utf-8')
     client_connection.send(chat_members)
 
     #allows the client to choose who they want to chat with
-    enter_ip = "please enter the IP Address of the person you'd like to chat with: ".encode('utf-8')
-    client_connection.send(enter_ip)
+    enter_name = "\nplease enter the name of the person you'd like to chat with:".encode('utf-8')
+    client_connection.send(enter_name)
     connected = False
     while(not connected):
         test_requested_member = None
-        test_requested_ip = client_connection.recv(1024).decode('utf-8')
+        test_requested_name= client_connection.recv(1024).decode('utf-8')
         is_member = False
         for member in chat_member_array:
-            if test_requested_ip == member.get_ip():
+            if test_requested_name == member.get_name():
+                client_connection.send(ACCEPTED_IP)
                 test_requested_member = member
                 is_member = True
+                connected = True
                 break
         if(not is_member):
-            not_member = f"{test_requested_ip} is not a valid ip address, please try again.".encode('utf-8')
+            client_connection.send(NOT_ACCEPTED_IP)
+            not_member = f"{test_requested_name} is not a valid name, please try again:".encode('utf-8')
             client_connection.send(not_member)
             continue
 
     #once the client is chosen, begin sending messages
     requested_member_name_ip = test_requested_member.get_name_and_ip()
-    starting_session = f'starting session with{requested_member_name_ip}...'
+    starting_session = f"\nstarting session with {requested_member_name_ip}...".encode('utf-8')
     client_connection.send(starting_session)
     test_requested_member.set_is_available(False)
     chat_member.set_is_available(False)
@@ -106,14 +116,18 @@ def start_session(requested_member, current_member):
 
 #main:
 def main():
-    server_socket = create_server()
-    print_information()
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"accepted client {client_address}")
-        temp = chat_member(client_socket,client_address)
-        chat_member_array.append(temp)
-        threading.Thread(target=handle_client, args=(temp,)).start()
+    try:
+        server_socket = create_server()
+        print_information()
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print(f"accepted client {client_address}")
+            temp = chat_member(client_socket,client_address)
+            chat_member_array.append(temp)
+            threading.Thread(target=handle_client, args=(temp,)).start()
+    except Exception as e:
+        server_socket.close()
+        print(e)
 
 main()
 
